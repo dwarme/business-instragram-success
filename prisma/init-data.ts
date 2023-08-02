54c897cab30d5c7787fd38ed8a810eec58ddc2f4
Daouda Warme <dwarme99@gmail.com>
2023-08-02 15:02:24 +0200

    prisma setup

--- prisma/init-data.ts
+++ prisma/init-data.ts
@@ -8,7 +8,7 @@ import {
   IUserCreateInput,
 } from "types";
 
-export const usersData: IUserCreateInput[] = [
+export const initUsersData: IUserCreateInput[] = [
   {
     email: "warme@instagram.com",
     // this is a hashed version of "twixrox"
@@ -27,7 +27,7 @@ export const usersData: IUserCreateInput[] = [
   },
 ];
 
-export const brandsData: {
+export const initBrandsData: {
   info: IBrandCreateInput;
   story: IBrandStoryCreateInput;
   stats: IBrandStatCreateInput[];
@@ -52,10 +52,60 @@ export const brandsData: {
       industry: "",
       region: "",
     },
-    story: {},
-    stats: [],
-    goal: {},
-    solutions: [],
-    creative: {},
+    story: {
+      title: "Classic fashion for all",
+      description:
+        "Reserved is the flagship fashion brand of LPP, the family-owned Polish company that also oversees fashion brands Sinsay, Mohito, Cropp and House. Established in Gdańsk over 30 years ago, the company now has more than 1,700 outlets in 38 countries.",
+      note: "Instagram is an important sales channel for Reserved and we are eager to implement any new functionality that Meta offers. After adding product tags to our Advantage+ catalogue ads, we saw meaningful results that led us to decide to include product tags in all our future campaigns.",
+      noteReference: "RADOSŁAW WOJCIECHOWSKI, ECOMMERCE DIRECTOR, RESERVED",
+    },
+    stats: [
+      {
+        title: "83%",
+        description:
+          "lower cost per incremental purchase with product tags added",
+      },
+      {
+        title: "6X",
+        description: "more incremental conversions with product tags added",
+      },
+      {
+        title: "6.1X",
+        description:
+          "higher incremental return on ad spend with product tags added",
+      },
+    ],
+    goal: {
+      title: "Increasing online sales",
+      description:
+        "Facing a challenging economic time for fashion retailers, Reserved wanted to increase ecommerce sales.",
+      note: "",
+      noteReference: "",
+    },
+    solutions: [
+      {
+        title: "Adding product tags",
+        contentHtml: `
+            Reserved was already using Advantage+ catalogue ads on Instagram, relying on ads in carousel format to display items from its large inventory of products. Advantage+ catalogue ads automatically show the right products to people who have expressed interest on a company’s website, app or elsewhere on the internet. Companies simply upload their product catalogue and set up their campaign, and it will continue working – showing people the most appropriate products, with up-to-date pricing and availability.
+            <br/><br/>
+            Reserved decided to add tags to the products that featured on their ads. Ads with product tags can encourage people to visit a product details page, where they can then shop for products from the ad. Businesses that use checkout on Instagram can enable checkout for a campaign to give shoppers the ability to purchase from the business's website or directly in-app.
+            <br/>
+            <br/>
+            To test the new approach, Reserved used a conversion lift test to measure its usual campaign of ads in carousel format against an identical campaign with product tags added. The difference in ad creative was subtle, but its effect was remarkable. Between September 28–October 12, 2022, the campaign achieved:
+            <ul class="_8grt" style=""><li class="_8-d2">83% lower cost per incremental purchase with product tags added</li><li class="_8-d2">6X more incremental conversions with product tags added</li><li class="_8-d2">6.1X higher incremental return on ad spend with product tags added</li></ul>
+        `,
+        mediaContentType: "IMAGE",
+        creativeType: "FEED",
+        imageUrl:
+          "https://scontent-mxp2-1.xx.fbcdn.net/v/t39.8562-6/342208166_232326462712736_3432463526407629652_n.jpg?stp=dst-webp&_nc_cat=104&ccb=1-7&_nc_sid=743b2f&_nc_ohc=4iWelyH7qSUAX8mhf5j&_nc_ht=scontent-mxp2-1.xx&oh=00_AfCj5mk0SYou7H59ew7YqESIsXRbLF9xupHVg1RkAT5P3Q&oe=64CE3C3E",
+        videoUrl: "",
+      },
+    ],
+    creative: {
+      title: "Reserved",
+      subtitle: "Tiny tags with big results",
+      imageUrl:
+        "https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.2365-6/335872802_624322782456872_6124428669723609543_n.jpg?stp=dst-webp&_nc_cat=105&ccb=1-7&_nc_sid=ad8a9d&_nc_ohc=qw64jdw6fs0AX_4q7bc&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfATHmGje7qSCFOEDtNqv7IUeEC5nKI3Zw7AS-ql1-kEXQ&oe=64E4800D",
+    },
   },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,91 @@
 import { PrismaClient } from "@prisma/client";
+import { initBrandsData, initUsersData } from "./init-data";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const daouda = await prisma.user.upsert({
-    where: { email: "warme-fake@instagram.com" },
-    update: {},
-    create: {
-      email: "warme-fake@instagram.com",
-      // this is a hashed version of "twixrox"
-      password: "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
-      firstName: "Daouda",
-      lastName: "Warme",
-      role: "ADMIN",
-    },
-  });
+  const users: any[] = [];
 
-  const lisa = await prisma.user.upsert({
-    where: { email: "lisa-fake@instagram.com" },
-    update: {},
-    create: {
-      email: "lisa-fake@instagram.com",
-      // this is a hashed version of "twixrox"
-      password: "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
-      firstName: "Lisa",
-      lastName: "Smith",
-      role: "USER",
-    },
-  });
+  for (const userData of initUsersData) {
+    users.push(
+      prisma.user.upsert({
+        where: { email: userData.email },
+        update: {},
+        create: {
+          email: userData.email,
+          password: userData.password,
+          lastName: userData.lastName,
+          firstName: userData.firstName,
+          role: userData.role,
+        },
+      })
+    );
+  }
+
+  await Promise.all(users);
+
+  for (const initBrandData of initBrandsData) {
+    const { info, story, stats, goal, solutions, creative } = initBrandData;
+    const brandStats: any[] = [];
+    const brandSolutions: any[] = [];
+
+    // Create brand
+    const brandCreated = await prisma.brand.create({
+      data: { ...info },
+    });
+
+    // Create story
+    const brandStoryCreated = await prisma.brandStory.create({
+      data: {
+        brandId: brandCreated.id,
+        ...story,
+      },
+    });
+
+    // Create stats
+    for (const stat of stats) {
+      brandStats.push(
+        prisma.brandStoryStat.create({
+          data: {
+            brandStoryId: brandCreated.id,
+            ...stat,
+          },
+        })
+      );
+    }
+
+    await Promise.all(brandStats);
+
+    // Create goal
+    const brandGoalCreatedData = await prisma.brandGoal.create({
+      data: {
+        brandId: brandCreated.id,
+        ...goal,
+      },
+    });
+
+    // Create solutions
+    for (const solution of solutions) {
+      brandSolutions.push(
+        prisma.brandSolution.create({
+          data: {
+            brandId: brandCreated.id,
+            ...solution,
+          },
+        })
+      );
+    }
+
+    await Promise.all(brandSolutions);
+
+    // Create creative
+    const creativeCreated = await prisma.creative.create({
+      data: {
+        brandId: brandCreated.id,
+        ...creative,
+      },
+    });
+  }
 }
 
 main()
