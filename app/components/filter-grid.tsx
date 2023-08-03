1236fdd481f534b06271ce5239dd7e4fde5bc94a
Daouda Warme <dwarme99@gmail.com>
2023-08-03 20:42:20 +0200

    filter

--- app/components/filter-grid.tsx
+++ app/components/filter-grid.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useMemo } from "react";
 import FilterCard from "./filter-card";
 import creativeFilters from "resources/creative-filters";
+import { useLocation } from "@remix-run/react";
+import { Link } from "react-router-dom";
 
 const FilterGrid: React.FC = () => {
   return (
@@ -23,10 +25,52 @@ const FilterGrid: React.FC = () => {
             ))}
           </div>
         </div>
-        <div className="_4mhg"></div>
+        <FilterOptionsSelectedPreview />
       </div>
     </div>
   );
 };
 
+const FilterOptionsSelectedPreview: React.FC = () => {
+  const { pathname, search } = useLocation();
+  const optionsSelected: { name: string; value: string }[] = useMemo(() => {
+    const selected: { name: string; value: string }[] = [];
+    const linkParams = new URLSearchParams(search);
+    for (const key of linkParams.keys()) {
+      selected.push({
+        name: key,
+        value: linkParams.get(key)!,
+      });
+    }
+
+    return selected;
+  }, [search]);
+
+  return (
+    <div className="_4mhg">
+      {optionsSelected.map((option) => {
+        const linkParams = new URLSearchParams(search);
+        linkParams.delete(option.name);
+        return (
+          <div key={option.name} className="_4mhh">
+            <Link
+              to={`${pathname}?${linkParams.toString()}`}
+              preventScrollReset
+              replace
+              className="_1avb _o5j"
+              data-ms='{"creative":"link","creative_detail":""}'
+              rel="async noreferrer"
+              role="button"
+            >
+              <i className="img sp_MOAVF_w7qFo_2x sx_0ae40a"></i>
+            </Link>
+            <p className="_5tdb _4yc6" data-ms='{"creative":"content"}'>
+              {option.value}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default FilterGrid;
